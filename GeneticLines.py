6df911a9de01a6e44b3da89f6d0ba4f8e5464db8
Zackary Ayoun <zackaryayoun@gmail.com>
2019-07-12 14:25:11 +0200

    It works!

--- GeneticLines.py
+++ GeneticLines.py
@@ -6,20 +6,35 @@ from PIL import Image, ImageDraw
 # TODO : Split file and clean later on
 
 # Constants : FIXME check if these should really be constants
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-POPSIZE = 100 #FIXME
-DNASIZE = 500 #FIXME
-MUTRATE = 10
+MAXSIZE = 50
+POPSIZE = 30 #FIXME
+DNASIZE = 400 #FIXME
+MUTRATE = 1000
 
 class Gene:
-    def __init__(self, from_pos, to_pos, color):
+    def __init__(self, x, y, xlen, ylen):
         # FIXME add asserts on the params?
-        self.from_pos = from_pos
-        self.to_pos = to_pos
-        self.color = color
+        self.x = x
+        self.y = y
+        self.xlen = xlen
+        self.ylen = ylen
+
+    def mutate(self, winsize):
+        if random.randint(0, MUTRATE) == 0:
+            self.x = random.randint(0, winsize[0] - 1)
+        if random.randint(0, MUTRATE) == 0:
+            self.y = random.randint(0, winsize[1] - 1)
+        if random.randint(0, MUTRATE / 2) == 0:
+            self.xlen = random.randint(0, MAXSIZE)
+        if random.randint(0, MUTRATE / 2) == 0:
+            self.ylen = random.randint(0, MAXSIZE)
 
     def draw(self, surface):
-        surface.line([self.from_pos, self.to_pos], fill=self.color, width=1)
+        from_pos = (self.x, self.y)
+        to_pos = (self.x + self.xlen, self.y + self.ylen)
+        surface.line([from_pos, to_pos], fill=BLACK, width=1)
 
 class Individual:
     def __init__(self, winsize):
@@ -27,13 +42,11 @@ class Individual:
         self.dna = []
         self.image = None
 
-    def addGene(self, from_pos, to_pos, color):
-        gene = Gene(from_pos, to_pos, color)
+    def addGene(self, x, y, xlen, ylen):
+        gene = Gene(x, y, xlen, ylen)
         self.dna.append(gene)
 
     def getImage(self):
-        if self.image is not None:
-            return
         self.image = Image.new("RGB", self.winsize, WHITE)
         surface = ImageDraw.Draw(self.image)
         for gene in self.dna:
@@ -49,37 +62,27 @@ class Individual:
             for y in range(self.winsize[1]):
                 r1, g1, b1 = image.getpixel((x, y))
                 r2, g2, b2 = self.image.getpixel((x, y))
-                r = 1 - (math.fabs(r1 - r2) / 255)
-                g = 1 - (math.fabs(g1 - g2) / 255)
-                b = 1 - (math.fabs(b1 - b2) / 255)
-                score += (r + g + b) / 3
-
-        score = score / (self.winsize[0] * self.winsize[1])
-        score *= 100
-
+                r = math.fabs(r1 - r2)
+                g = math.fabs(g1 - g2)
+                b = math.fabs(b1 - b2)
+                score += (r + g + b)
         return score
 
     def mutate(self):
+        # FIXME below isn't good
         for i in range(DNASIZE):
-            if random.randint(0, 100) < MUTRATE:
-                    from_pos = (random.randint(0, self.winsize[0] - 1),
-                                random.randint(0, self.winsize[1] - 1))
-                    to_pos = (random.randint(0, self.winsize[0] - 1),
-                              random.randint(0, self.winsize[1] - 1))
-                    color = (random.randint(0, 255),
-                             random.randint(0, 255),
-                             random.randint(0, 255))
-                    self.dna[i] = Gene(from_pos, to_pos, color)
+            self.dna[i].mutate(self.winsize)
 
     def cross(self, ind):
         ret = Individual(self.winsize)
         for i in range(DNASIZE):
             gene = self.dna[i] if random.randint(0, 1) == 0 \
                                else ind.dna[i]
-            ret.dna.append(gene)
+            ret.addGene(gene.x, gene.y, gene.xlen, gene.ylen)
         return ret
 
     def save(self):
+        # FIXME there's probably a problem with that, use pygame instead
         if self.image is None:
             self.getImage()
 
@@ -94,22 +97,19 @@ class Population:
             for i in range(POPSIZE):
                 ind = Individual(self.winsize)
                 for j in range(DNASIZE):
-                    from_pos = (random.randint(0, self.winsize[0] - 1),
-                                random.randint(0, self.winsize[1] - 1))
-                    to_pos = (random.randint(0, self.winsize[0] - 1),
+                    (x, y) = (random.randint(0, self.winsize[0] - 1),
                               random.randint(0, self.winsize[1] - 1))
-                    color = (random.randint(0, 255),
-                             random.randint(0, 255),
-                             random.randint(0, 255))
-                    ind.addGene(from_pos, to_pos, color)
+                    (xlen, ylen) = (random.randint(0, MAXSIZE), #FIXME add a const for the max size?
+                                    random.randint(0, MAXSIZE))
+                    ind.addGene(x, y, xlen, ylen)
                 self.pop.append({"individual": ind})
         else:
-            for i in range(POPSIZE / 2):
-                for _ in range(2):
-                    #FIXME except i itself?
+            for i in range(POPSIZE):
                     self.pop.append({"individual":
-                        previous.getPop()[i]["individual"].cross(previous.getPop()[random.randint(0, POPSIZE/2)]["individual"])})
+                        self.goodEnoughIndividual(previous).cross(
+                            self.goodEnoughIndividual(previous))})
 
+    def mutate(self):
         for i in self.pop:
             i["individual"].mutate()
 
@@ -118,7 +118,7 @@ class Population:
         for i in self.pop:
             ind = i["individual"]
             score = ind.getScore(image)
-            i["score"] = score
+            i["score"] = score #FIXME score should be within the ind class?
             self.average += score
         self.average /= POPSIZE
 
@@ -126,6 +126,12 @@ class Population:
         if image is not None:
             self.computeScores(image)
         self.pop = sorted(self.pop, key=lambda k: k["score"], reverse=True)
+
+    def goodEnoughIndividual(self, pop):
+        for i in pop.pop:
+            if random.randint(0, 1) == 0:
+                return i["individual"]
+        return pop.pop[0]["individual"]
 
     def getPop(self):
         return self.pop
@@ -138,6 +144,7 @@ class World:
         random.seed()
         self.running = True
         self.image = Image.open(path)
+        self.image = self.image.convert('1')
         self.image = self.image.convert("RGB")
         self.winsize = self.image.size
 
@@ -146,8 +153,9 @@ class World:
         while self.running:
             pop.sort(self.image)
             print pop.getAverage()
-            pop = Population(self.winsize, previous=pop)
             pop.getPop()[0]["individual"].save()
+            pop = Population(self.winsize, previous=pop)
+            pop.mutate()
 
 if __name__ == '__main__':
     assert (len(sys.argv) == 2) #FIXME maybe add a message
